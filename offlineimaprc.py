06e8e5785bbc6aabbdc4381d572e01f3ab7b9e17
Alexander Kolosov <akolosov@cs.petrsu.ru>
2016-06-21 16:17:18 +0300

    Password extracting function was debugged

--- offlineimaprc.py
+++ offlineimaprc.py
@@ -2,14 +2,13 @@
 import gnupg, re
 from os.path import expanduser
 
+# Extract password from .authinfo. Decrypt file if GPG-encrypted.
 def get_authinfo_password(filename, machine, login):
     authinfo_file = expanduser(filename)
 
     s = "machine\s+%s\s+login\s+%s\s+password\s+(.+)" % (machine, login)
     p = re.compile(s)    
 
-    password = ""
-    
     with open(authinfo_file, "rb") as stream:
         # if authinfo file is GPG-encrypted
         if re.search('\.gpg$', authinfo_file):
@@ -18,8 +17,6 @@ def get_authinfo_password(filename, machine, login):
         else:
             authinfo = stream.read()
 
-        # Take password either from decrypted or from unencrypted
-        # stream
-        password = p.search(authinfo).group(1)
-
-    return password
+    # Take password either from decrypted or from unencrypted
+    # stream
+    return p.search(authinfo).group(1)
