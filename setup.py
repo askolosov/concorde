b459233a8af26cb1a62af912b19a66ab397c4997
Alexander Kolosov <alexander@kolosov.xyz>
2016-06-24 21:18:14 +0300

    Minimal setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Concorde',
+      version='0.1',
+      description='Notmuch tag management tool',
+      author='Alexander Kolosov',
+      author_email='alexander@kolosov.xyz',
+      url='https://github.com/askolosov/concorde',
+      packages=['concorde'],
+     )
