ebec812ac45e21ccfbd473ae73838b0674088352
Alexander Kolosov <alexander@kolosov.xyz>
2016-06-26 13:58:27 +0300

    Do several attempts of opening notmuch database

--- concorde/__main__.py
+++ concorde/__main__.py
@@ -1,10 +1,30 @@
 import notmuch
 import concorde.instructions as instr
 import sys
+import time
+from . import logger
 
 if len(sys.argv) < 2:
     print("Usage: %s <filename>" % sys.argv[0])
     sys.exit(1)
 
-with notmuch.Database(mode=notmuch.Database.MODE.READ_WRITE) as db:
+max_tries = 3
+wait_interval = 1
+i = 0
+db = None
+
+while i < max_tries and not db:
+    i += 1
+    
+    try:
+        db = notmuch.Database(mode=notmuch.Database.MODE.READ_WRITE)
+    except notmuch.NotmuchError:
+        logger.warning("Failed to open notmuch database. Waiting for %d seconds for another attempt...", wait_interval)
+        time.sleep(wait_interval)
+
+if not db:
+    logger.error("Notmuch database couldn't be opened after %d attempts.", i)
+    sys.exit(1)
+        
+with db:
     instr.process_file(db, sys.argv[1])
